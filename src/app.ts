1c9e2705ee3a2fd651baf94087b6da06696a9eff
hugo <hugo.nextpay@gamil.com>
2025-05-29 10:16:45 +0900

    cors추가가

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
-import express from 'express';
+import express, { Request } from 'express';
 import dotenv from 'dotenv';
 import logger from './config/logger.config';
 import router from './router';
 import errorHandler from './config/error.config';
 import redis from './config/redis.config';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// cors
+// cors
+app.use(cors());
+
 
 app.use(express.urlencoded({ limit: "300mb", extended: false }));
 app.use(express.json({ limit: "300mb" }));
@@ -18,6 +24,11 @@ app.use(express.json({ limit: "300mb" }));
 app.use('/ping', (_,res,_n)=>{
   res.send('pong');
 });
+
+app.head('/',(req : Request,res,next)=>{
+    logger.debug(req.headers['user-agent']);
+    res.send();
+})
 
 app.use('/api', router);
 
